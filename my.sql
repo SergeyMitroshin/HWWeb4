51e50f445ab1389088e8820bf37a45d9756848e6
SergeyMitroshin <sergey@host-best.com>
2023-01-21 23:21:36 +0300

    v02

--- my.sql
+++ my.sql
@@ -2,7 +2,7 @@
 
 
 -- create
-CREATE TABLE STUDENTS (
+CREATE TABLE students (
   id INTEGER PRIMARY KEY,
   name TEXT NOT NULL,
   age INTEGER NOT NULL,
@@ -10,15 +10,15 @@ CREATE TABLE STUDENTS (
 );
 
 -- insert
-INSERT INTO STUDENTS VALUES (0001, 'Вася', 17, 'Москва, Ленинский проспект, 24, кв 4');
-INSERT INTO STUDENTS VALUES (0002, 'Петя', 19, 'Пермь, Попова ул, 22, кв 4');
-INSERT INTO STUDENTS VALUES (0003, 'Таня', 46, 'Тула, Гоголевская ул, 33, кв 4');
-INSERT INTO STUDENTS VALUES (0004, 'Маня', 22, 'Москва, Остоженка ул, 15, кв 145');
-INSERT INTO STUDENTS VALUES (0005, 'Барсик', 33, 'Москва, Графскиё пер, 15, кв 4');
-INSERT INTO STUDENTS VALUES (0006, 'Жучка', 50, 'Калининград, Толстого ул, 22, кв 1');
-INSERT INTO STUDENTS VALUES (0007, 'Вася', 34, 'Тверь, Линейная ул, 22, кв 4');
-INSERT INTO STUDENTS VALUES (0008, 'Николай', 21, 'Москва, Кутузовский проспект, 16, кв 44');
+INSERT INTO students VALUES (0001, 'Вася', 17, 'Москва, Ленинский проспект, 24, кв 4');
+INSERT INTO students VALUES (0002, 'Петя', 19, 'Пермь, Попова ул, 22, кв 4');
+INSERT INTO students VALUES (0003, 'Таня', 46, 'Тула, Гоголевская ул, 33, кв 4');
+INSERT INTO students VALUES (0004, 'Маня', 22, 'Москва, Остоженка ул, 15, кв 145');
+INSERT INTO students VALUES (0005, 'Барсик', 33, 'Москва, Графскиё пер, 15, кв 4');
+INSERT INTO students VALUES (0006, 'Жучка', 50, 'Калининград, Толстого ул, 22, кв 1');
+INSERT INTO students VALUES (0007, 'Вася', 34, 'Тверь, Линейная ул, 22, кв 4');
+INSERT INTO students VALUES (0008, 'Николай', 21, 'Москва, Кутузовский проспект, 16, кв 44');
 
 -- fetch 
-SELECT * FROM STUDENTS WHERE address LIKE '%Москва%' AND age >= 18 AND age < 30 ;
+SELECT name FROM students WHERE address LIKE '%Москва%' AND age >= 18 AND age < 30;
 
